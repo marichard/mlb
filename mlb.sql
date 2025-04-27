0b9756548197358853430d70b26d4ad0b82e7ea6
Marcus Richard <120982974+marichard@users.noreply.github.com>
2025-04-26 22:53:29 -0700

    objective 1 questions to be answered

--- mlb.sql
+++ mlb.sql
@@ -1,3 +1,10 @@
+-- How many schools were there that produced MLB players in each decade?
+-- What are the names of the top 5 schools that produced the most players?
+-- What were the names of the top 3 schools that produced the most players in each decade?
 SELECT
     *
-FROM players;
+FROM school_details;
+
+SELECT
+    *
+FROM schools;
