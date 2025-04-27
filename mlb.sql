03864a3540add9860585fc3febf00d2e4ca64cf3
Marcus Richard <120982974+marichard@users.noreply.github.com>
2025-04-27 16:54:51 -0700

    objective 1.1 answered

--- mlb.sql
+++ mlb.sql
@@ -1,6 +1,8 @@
+-- Objective 1
 -- How many schools were there that produced MLB players in each decade?
 -- What are the names of the top 5 schools that produced the most players?
 -- What were the names of the top 3 schools that produced the most players in each decade?
+-- lay of the land
 SELECT
     *
 FROM school_details;
@@ -8,3 +10,18 @@ FROM school_details;
 SELECT
     *
 FROM schools;
+
+SELECT
+    *
+FROM players;
+
+SELECT
+    *
+FROM salaries;
+
+SELECT
+    ROUND(yearid, -1) AS decade,
+    COUNT(DISTINCT schoolid) AS school_count
+FROM schools
+GROUP BY decade
+ORDER BY decade DESC;
