06346982985471d7544e0f096cac2104abf84aa0
Marcus Richard <120982974+marichard@users.noreply.github.com>
2025-04-29 16:56:53 -0700

    Objective 1.3 complete

--- mlb.sql
+++ mlb.sql
@@ -36,5 +36,27 @@ ORDER BY players_count DESC
 LIMIT 5;
 
 -- What were the names of the top 3 schools that produced the most players in each decade?
+WITH ds AS (SELECT
+            ROUND(s.yearid, -1) AS decade,
+            sd.name_full AS school_name,
+            COUNT(DISTINCT playerid) AS players_count
+            FROM schools s
+            LEFT JOIN school_details sd
+                ON s.schoolid = sd.schoolid
+            GROUP BY school_name, decade),
 
+    rn AS (SELECT
+        decade,
+        school_name,
+        players_count,
+        ROW_NUMBER() OVER (PARTITION BY decade ORDER BY players_count DESC) AS row_num
+    FROM ds)
 
+SELECT
+    decade,
+    school_name,
+    players_count
+FROM rn
+WHERE row_num <= 3
+ORDER BY decade DESC,
+         row_num;
